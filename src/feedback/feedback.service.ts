2c53b5e65bba3609b1a0979d164d41323bca0f8a
Kamoliddinbek <kamoliddinbekxudoyberdi@gmail.com>
2025-12-10 21:18:48 +0500

    bot product link issue fixed

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class FeedbackService {
   constructor(private prisma: PrismaService) {}
 
   async createNotFound(input: {
-    query: string
-    pageUrl?: string
-    userAgent?: string
-    ip?: string
-    userId?: number
+    query: string;
+    pageUrl?: string;
+    userAgent?: string;
+    ip?: string;
+    userId?: number;
   }) {
     return await this.prisma.notFoundFeedback.create({
       data: {
@@ -20,7 +20,6 @@ export class FeedbackService {
         ip: input.ip || null,
         userId: input.userId || null,
       },
-    })
+    });
   }
 }
-
